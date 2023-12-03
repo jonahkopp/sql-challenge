11d27cce737fe13addf544b82eab2f8fd1a3a33d
Jonah Kopp <jonah.kopp123@gmail.com>
2023-12-03 13:04:43 -0700

    added sql code to create tables and relationships

---
+++ EmployeeSQL/create_tables.sql
@@ -0,0 +1,79 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/WEb9JC
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+drop table if exists dept_manager;
+drop table if exists dept_emp;
+drop table if exists departments;
+drop table if exists salaries;
+drop table if exists employees;
+drop table if exists titles;
+
+CREATE TABLE employees (
+    emp_no INTEGER NOT NULL,
+    emp_title_id VARCHAR(30),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex VARCHAR(1),
+    hire_date DATE,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL,
+    salary INTEGER,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(30) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    dept_emp_no VARCHAR(50) NOT NULL,
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        dept_emp_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no
+     )
+);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
